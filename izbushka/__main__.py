3f4204254215a3d4f6e3a676beb2e8e7a4622d96
Anton Evdokimov <meowmeowcode@gmail.com>
2022-12-20 16:56:55 +0400

    add the CLI

---
+++ izbushka/__main__.py
@@ -0,0 +1,41 @@
+import os
+
+import yaml
+
+from . import cli
+from .entities import Config
+from .history_repo import HistoryDBRepo
+from .migrations_repo import MigrationsPackageRepo
+from .migrations_service import MigrationsService
+from .operations import ClickHouseConnectOperations
+
+
+try:
+    with open("izbushka.yml") as f:
+        conf = yaml.safe_load(f)
+except FileNotFoundError:
+    cli.init()
+
+
+config = Config(
+    host=os.getenv("CLICKHOUSE_HOST") or conf.get("host") or "localhost",
+    port=int(os.getenv("CLICKHOUSE_PORT") or conf.get("port") or 8123),
+    username=os.getenv("CLICKHOUSE_USERNAME") or conf.get("username") or "Default",
+    password=os.getenv("CLICKHOUSE_PASSWORD") or conf.get("password") or "",
+    database=os.getenv("CLICKHOUSE_DATABASE") or conf.get("database") or "default",
+    interface=os.getenv("CLICKHOUSE_INTERFACE") or conf.get("interface") or "http",
+    cluster=os.getenv("CLICKHOUSE_CLUSTER") or conf.get("cluster"),
+)
+
+operations = ClickHouseConnectOperations(config)
+history_repo = HistoryDBRepo(operations)
+migrations_repo = MigrationsPackageRepo(conf["migrations_package"])
+
+migrations_service = MigrationsService(
+    operations=operations,
+    history_repo=history_repo,
+    migrations_repo=migrations_repo,
+)
+
+cmd = cli.build_cmd(migrations_service=migrations_service)
+cmd()
---
+++ izbushka/cli.py
@@ -0,0 +1,49 @@
+from typing import Optional
+
+import click
+import yaml
+from click import Group
+
+from .entities import MigrationType
+from .errors import OperationError
+from .migrations_service import MigrationsService
+
+
+@click.command
+@click.argument("migrations_package")
+def init(migrations_package: str) -> None:
+    with click.open_file("izbushka.yml", "w") as f:
+        yaml.safe_dump({"migrations_package": migrations_package}, f)
+
+
+def build_cmd(migrations_service: MigrationsService) -> Group:
+    @click.group
+    def cmd() -> None:
+        pass
+
+    @cmd.command
+    def run() -> None:
+        click.echo("Applying migrations...", nl=False)
+        migrations_service.run()
+        click.echo(" [DONE]")
+
+    @cmd.command
+    def status() -> None:
+        status = migrations_service.get_status()
+        click.echo(yaml.safe_dump(status))
+
+    @cmd.command
+    @click.argument("name")
+    @click.option("--version", "-v")
+    @click.option("--type", "-t")
+    def add(name: str, version: Optional[str], type: Optional[str]) -> None:
+        try:
+            migrations_service.add(
+                name=name,
+                version=version,
+                type_=None if type is None else MigrationType[type],
+            )
+        except OperationError as err:
+            click.echo(str(err), err=True)
+
+    return cmd
